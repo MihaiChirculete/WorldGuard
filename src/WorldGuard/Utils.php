182ae725a7741996586b3abadde6a2e5daa4008d
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-03-16 01:59:17 +0400

    Add badass creativity.

--- src/WorldGuard/Utils.php
+++ src/WorldGuard/Utils.php
@@ -1,4 +1,26 @@
 <?php
+
+/**
+*
+*  _     _  _______  ______    ___      ______   _______  __   __  _______  ______    ______  
+* | | _ | ||       ||    _ |  |   |    |      | |       ||  | |  ||   _   ||    _ |  |      | 
+* | || || ||   _   ||   | ||  |   |    |  _    ||    ___||  | |  ||  |_|  ||   | ||  |  _    |
+* |       ||  | |  ||   |_||_ |   |    | | |   ||   | __ |  |_|  ||       ||   |_||_ | | |   |
+* |       ||  |_|  ||    __  ||   |___ | |_|   ||   ||  ||       ||       ||    __  || |_|   |
+* |   _   ||       ||   |  | ||       ||       ||   |_| ||       ||   _   ||   |  | ||       |
+* |__| |__||_______||___|  |_||_______||______| |_______||_______||__| |__||___|  |_||______| 
+*
+* By Muqsit Rayyan.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Twitter: @muqsitrayyan
+* GitHub: https://github.com/Muqsit
+*/
+
 namespace WorldGuard;
 
 class Utils {
