0b6e0405e19559a4cfd11f66507c6be88f8b1d7a
Chalapa13 <mihai.chirculete98@gmail.com>
2019-09-30 09:31:17 +0300

    Added a vardump for debugging the save of regions.

--- src/Chalapa13/WorldGuard/WorldGuard.php
+++ src/Chalapa13/WorldGuard/WorldGuard.php
@@ -349,6 +349,7 @@ class WorldGuard extends PluginBase {
     public function saveRegions(){
         $data = [];
         foreach ($this->regions as $name => $region) {
+            var_dump($region);
             $data[$name] = $region->toArray();
         }
         yaml_emit_file($this->getDataFolder().'regions.yml', $data);
