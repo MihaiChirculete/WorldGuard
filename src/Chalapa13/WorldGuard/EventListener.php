6b72a972808faf22b6c007e36e2563edbd16c6c6
Chalapa <mihai.chirculete98@gmail.com>
2020-05-21 13:22:33 +0300

    Removed unused code.

--- src/Chalapa13/WorldGuard/EventListener.php
+++ src/Chalapa13/WorldGuard/EventListener.php
@@ -60,13 +60,6 @@ class EventListener implements Listener {
     public function __construct(WorldGuard $plugin)
     {
         $this->plugin = $plugin;
-
-        if($plugin->pureEntitiesPlugin !== null)
-            $plugin->getServer()->getPluginManager()->registerEvent(CreatureSpawnEvent::class, $this, 3, new MethodEventExecutor("onCreatureSpawn"), $plugin, false);
-
-        /*
-        if($plugin->getServer()->getPluginManager()->getPlugin("PureEntitiesX") !== null)
-            $plugin->getServer()->getPluginManager()->registerEvent(CreatureSpawnEvent::class, $this, 3, new MethodEventExecutor("onCreatureSpawn"), $plugin, false);*/
     }
 
     /**
