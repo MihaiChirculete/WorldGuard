6bbc2f96d2545b05c234127183762f2045fa4fdf
Chalapa <mihai.chirculete98@gmail.com>
2020-05-21 10:05:30 +0300

    Added updating functionality for resource files.

--- src/Chalapa13/WorldGuard/ResourceUtils/ResourceManager.php
+++ src/Chalapa13/WorldGuard/ResourceUtils/ResourceManager.php
@@ -10,9 +10,10 @@ class ResourceManager
 {
     /** Only 1 instance of this class will be allowed at all times */
     private static $instance = null;
+    private $resUpdaterInstance = null;
     private $pluginInstance = null;
     private $serverInstance = null;
-    private $pluginVersion = "1.1.0.1";
+    private $pluginVersion = null;
     private $messages = [];
     private $lang = [];
     private $config = [];
@@ -23,6 +24,9 @@ class ResourceManager
     {
         $this->pluginInstance = $plugin;
         $this->serverInstance = $sv;
+        $this->resUpdaterInstance = ResourceUpdater::getInstance($this);
+
+        $this->pluginVersion = $this->pluginInstance->getDescription()->getVersion();
     }
 
     public static function getInstance(WorldGuard $plugin, Server $sv)
@@ -37,6 +41,8 @@ class ResourceManager
     public function getLanguagePack() { return $this->lang; }
     public function getMessages() { return $this->messages; }
     public function getRegions() : array { return $this->regions; }
+
+    public function getPluginVersion() { return $this->pluginVersion; }
 
     public function getConfigVersion()
     {
@@ -106,11 +112,7 @@ class ResourceManager
         if (is_file($path.'config.yml')) {
             $this->config = yaml_parse_file($path.'config.yml');
         } else {
-            $this->config = array(
-                "version" => $this->pluginVersion,
-                "language" => "en",
-                "debugging" => false
-            );
+            $this->config = $this->resUpdaterInstance->getDefaultConfig();
 
             yaml_emit_file($path.'config.yml', $this->config);
         }
@@ -128,60 +130,7 @@ class ResourceManager
             $this->config["language"] = "en";
             yaml_emit_file($path.'config.yml', $this->config);
 
-            $this->lang = array(
-                "version" => $this->pluginVersion,
-                "author_name" => "Chalapa",
-                "gui_wg_menu_title" => "World Guard Menu",
-                "gui_label_choose_option" => "Choose an option",
-                "gui_btn_rg_management" => "Region Management",
-                "gui_btn_help" => "Help",
-                "gui_btn_manage_existing" => "Manage existing region",
-                "gui_btn_create_region" => "Create a new region",
-                "gui_btn_redefine_region" => "Redefine a region",
-                "gui_btn_delete_region" => "Delete a region",
-                "gui_creation_menu_title" => "Region Creation",
-                "gui_creation_menu_label1" => "Let's help you create a region.",
-                "gui_creation_menu_rg_name_box" => "First you will have to enter a name for your region.",
-                "gui_creation_menu_label2" => "If you want your region to expand infinitely upwards and downards check the following option.",
-                "gui_creation_menu_toggle_expand" => "Expand vertically",
-                "gui_creation_menu_label3" => "Now hit the §a'Submit'§r and select 2 corners of your region as you will be instructed next.",
-                "gui_dropdown_select_redefine" => "Select the region you would like to redefine",
-                "gui_dropdown_select_delete" => "Select the region you would like to delete",
-                "gui_dropdown_select_manage" => "Select the region you would like to manage",
-                "gui_manage_menu_title" => "Managing region:",
-                "gui_flag_pvp" => "PvP",
-                "gui_flag_xp_drops" => "Experience drops",
-                "gui_flag_invincible" => "Invincible",
-                "gui_flag_fall_dmg" => "Fall damage",
-                "gui_flag_usage" => "Use",
-                "gui_flag_item_drop" => "Item drop",
-                "gui_flag_explosions" => "Explosions",
-                "gui_flag_notify_enter" => "Notify enter",
-                "gui_flag_notify_leave" => "Notify leave",
-                "gui_flag_potions" => "Allow potions",
-                "gui_flag_allowed_enter" => "Allowed enter",
-                "gui_flag_allowed_leave" => "Allowed leave",
-                "gui_flag_gm" => "Gamemode",
-                "gui_gm_survival" => "Survival",
-                "gui_gm_creative" => "Creative",
-                "gui_flag_sleep" => "Allow sleeping",
-                "gui_flag_send_chat" => "Allow sending chat messages",
-                "gui_flag_rcv_chat" => "Allow receiving chat messages",
-                "gui_flag_enderpearl" => "Allow use of ender pearls",
-                "gui_flag_fly_mode" => "Fly mode",
-                "gui_enabled" => "Enabled",
-                "gui_disabled" => "Disabled",
-                "gui_flag_eat" => "Allow eating",
-                "gui_flag_dmg_animals" => "Allow damaging of animals",
-                "gui_flag_dmg_monsters" => "Allow damaging of monsters",
-                "gui_flag_leaf_decay" => "Allow leaf decay",
-                "gui_flag_plant_growth" => "Allow plant growth",
-                "gui_flag_spread" => "Allow spreading",
-                "gui_flag_block_burn" => "Allow block burn",
-                "gui_flag_priority" => "Region priority",
-                "gui_help_menu_label1" => "If you need help setting up world guard check out the tutorial we made for you:",
-                "gui_help_menu_label2" => "http://worldguard.ddns.net/tutorial"
-            );
+            $this->lang = $this->resUpdaterInstance->getDefaultLanguagePack();
 
             yaml_emit_file($path.'lang_en.yml', $this->lang);
         }
@@ -197,24 +146,34 @@ class ResourceManager
             $this->messages = yaml_parse_file($path.'messages.yml');
         }
         else{
-            $this->messages = array (
-                "version" => $this->pluginVersion,
-                "denied-enter" => "You cannot enter this area.",
-                "denied-leave" => "You cannot leave this area.",
-                "no-permission-for-command" => "You do not have permission to use this command.",
-                "denied-eat" => "You cannot eat in this area.",
-                "denied-ender-pearls" => "You cannot use ender pearls in this area.",
-                "denied-chat" => "You cannot chat in this region.",
-                "denied-item-drop" => "You cannot drop items in this region.",
-                "denied-pvp" => "You cannot hurt players of this region.",
-                "denied-block-break" => "You cannot break blocks in this region.",
-                "denied-block-place" => "You cannot place blocks in this region.",
-                "denied-hurt-animal" => "You cannot hurt animals of this region.",
-                "denied-hurt-monster" => "You cannot hurt monsters of this region."
-            );
+            $this->messages = $this->resUpdaterInstance->getDefaultMessages();
 
             yaml_emit_file($path.'messages.yml', $this->messages);
         }
+    }
+
+    public function saveConfig($config)
+    {
+        $this->config = $config;
+
+        $path = $this->pluginInstance->getDataFolder();
+        yaml_emit_file($path.'config.yml', $this->config);
+    }
+
+    public function saveMessages($messages)
+    {
+        $this->messages = $messages;
+
+        $path = $this->pluginInstance->getDataFolder();
+        yaml_emit_file($path.'messages.yml', $this->messages);
+    }
+
+    public function saveLanguagePack($langPack)
+    {
+        $this->lang = $langPack;
+
+        $path = $this->pluginInstance->getDataFolder();
+        yaml_emit_file($path . "lang_" . $this->config["language"] . ".yml", $this->lang);
     }
 
 }
--- src/Chalapa13/WorldGuard/ResourceUtils/ResourceUpdater.php
+++ src/Chalapa13/WorldGuard/ResourceUtils/ResourceUpdater.php
@@ -13,9 +13,89 @@ class ResourceUpdater
     private static $instance = null;
     private $resouceManagerInstance = null;
 
+    private $defaultConfig = null;
+    private $defaultMessages = null;
+    private $defaultLanguagePack = null;
+
     private function __construct(ResourceManager $resouceManagerInstance)
     {
         $this->resouceManagerInstance = $resouceManagerInstance;
+
+        $this->defaultConfig = array(
+            "version" => $this->resourceManagerInstance->getPluginVersion(),
+            "language" => "en",
+            "debugging" => false);
+
+        $this->defaultMessages = array (
+            "version" => $this->resourceManagerInstance->getPluginVersion(),
+            "denied-enter" => "You cannot enter this area.",
+            "denied-leave" => "You cannot leave this area.",
+            "no-permission-for-command" => "You do not have permission to use this command.",
+            "denied-eat" => "You cannot eat in this area.",
+            "denied-ender-pearls" => "You cannot use ender pearls in this area.",
+            "denied-chat" => "You cannot chat in this region.",
+            "denied-item-drop" => "You cannot drop items in this region.",
+            "denied-pvp" => "You cannot hurt players of this region.",
+            "denied-block-break" => "You cannot break blocks in this region.",
+            "denied-block-place" => "You cannot place blocks in this region.",
+            "denied-hurt-animal" => "You cannot hurt animals of this region.",
+            "denied-hurt-monster" => "You cannot hurt monsters of this region."
+        );
+
+        $this->defaultLanguagePack = array(
+            "version" => $this->resourceManagerInstance->getPluginVersion(),
+            "author_name" => "Chalapa",
+            "gui_wg_menu_title" => "World Guard Menu",
+            "gui_label_choose_option" => "Choose an option",
+            "gui_btn_rg_management" => "Region Management",
+            "gui_btn_help" => "Help",
+            "gui_btn_manage_existing" => "Manage existing region",
+            "gui_btn_create_region" => "Create a new region",
+            "gui_btn_redefine_region" => "Redefine a region",
+            "gui_btn_delete_region" => "Delete a region",
+            "gui_creation_menu_title" => "Region Creation",
+            "gui_creation_menu_label1" => "Let's help you create a region.",
+            "gui_creation_menu_rg_name_box" => "First you will have to enter a name for your region.",
+            "gui_creation_menu_label2" => "If you want your region to expand infinitely upwards and downards check the following option.",
+            "gui_creation_menu_toggle_expand" => "Expand vertically",
+            "gui_creation_menu_label3" => "Now hit the §a'Submit'§r and select 2 corners of your region as you will be instructed next.",
+            "gui_dropdown_select_redefine" => "Select the region you would like to redefine",
+            "gui_dropdown_select_delete" => "Select the region you would like to delete",
+            "gui_dropdown_select_manage" => "Select the region you would like to manage",
+            "gui_manage_menu_title" => "Managing region:",
+            "gui_flag_pvp" => "PvP",
+            "gui_flag_xp_drops" => "Experience drops",
+            "gui_flag_invincible" => "Invincible",
+            "gui_flag_fall_dmg" => "Fall damage",
+            "gui_flag_usage" => "Use",
+            "gui_flag_item_drop" => "Item drop",
+            "gui_flag_explosions" => "Explosions",
+            "gui_flag_notify_enter" => "Notify enter",
+            "gui_flag_notify_leave" => "Notify leave",
+            "gui_flag_potions" => "Allow potions",
+            "gui_flag_allowed_enter" => "Allowed enter",
+            "gui_flag_allowed_leave" => "Allowed leave",
+            "gui_flag_gm" => "Gamemode",
+            "gui_gm_survival" => "Survival",
+            "gui_gm_creative" => "Creative",
+            "gui_flag_sleep" => "Allow sleeping",
+            "gui_flag_send_chat" => "Allow sending chat messages",
+            "gui_flag_rcv_chat" => "Allow receiving chat messages",
+            "gui_flag_enderpearl" => "Allow use of ender pearls",
+            "gui_flag_fly_mode" => "Fly mode",
+            "gui_enabled" => "Enabled",
+            "gui_disabled" => "Disabled",
+            "gui_flag_eat" => "Allow eating",
+            "gui_flag_dmg_animals" => "Allow damaging of animals",
+            "gui_flag_dmg_monsters" => "Allow damaging of monsters",
+            "gui_flag_leaf_decay" => "Allow leaf decay",
+            "gui_flag_plant_growth" => "Allow plant growth",
+            "gui_flag_spread" => "Allow spreading",
+            "gui_flag_block_burn" => "Allow block burn",
+            "gui_flag_priority" => "Region priority",
+            "gui_help_menu_label1" => "If you need help setting up world guard check out the tutorial we made for you:",
+            "gui_help_menu_label2" => "http://worldguard.ddns.net/tutorial"
+        );
     }
 
     public static function getInstance(ResourceManager $resouceManagerInstance)
@@ -35,7 +115,7 @@ class ResourceUpdater
         if($ver === null)
             return true;
 
-        if($ver !== $this->pluginVersion)
+        if($ver !== $this->resourceManagerInstance->getPluginVersion())
             return true;
 
         return false;
@@ -49,7 +129,7 @@ class ResourceUpdater
         if($ver === null)
             return true;
 
-        if($ver !== $this->pluginVersion)
+        if($ver !== $this->resourceManagerInstance->getPluginVersion())
             return true;
 
         return false;
@@ -63,16 +143,69 @@ class ResourceUpdater
         if($ver === null)
             return true;
 
-        if($ver !== $this->pluginVersion)
+        if($ver !== $this->resourceManagerInstance->getPluginVersion())
             return true;
 
         return false;
     }
     /****************************************************************** */
 
-    /** TO-DO: Code this function */
+    public function getDefaultConfig() { return $this->defaultConfig; }
+    public function getDefaultMessages() { return $this->defaultMessages; }
+    public function getDefaultLanguagePack() { return $this->defaultLanguagePack; }
+
+    /** For each resource file check it's version and if it doesn't match have it updated */
     public function updateResourcesIfRequired()
     {
+        if($this->isConfigResourceOutdated())
+        {
+            $oldConfig = $this->resouceManagerInstance->getConfig();
 
+            $newConfigKeys = array_keys($oldConfig);
+
+            /** If a key from the new config is not present in the old config, then add it */
+            foreach ($newConfigKeys as $key)
+            {
+                if(!isset($oldConfig[$key]))
+                    $oldConfig[$key] = $this->getDefaultConfig()[$key];
+            }
+
+            /** Change the file version to match the current version */
+            $oldConfig['version'] = $this->resouceManagerInstance->getPluginVersion();
+
+            $this->resouceManagerInstance->saveConfig($oldConfig);
+        }
+
+        if($this->isMessagesResourceOutdated())
+        {
+            $oldMessages = $this->resouceManagerInstance->getMessages();
+
+            $newMessagesKeys = array_keys($oldMessages);
+            foreach ($newMessagesKeys as $key)
+            {
+                if(!isset($oldMessages[$key]))
+                    $oldMessages[$key] = $this->getDefaultMessages()[$key];
+            }
+
+            $oldMessages['version'] = $this->resouceManagerInstance->getPluginVersion();
+
+            $this->resouceManagerInstance->saveMessages($oldMessages);
+        }
+
+        if($this->isLanguagePackResourceOutdated())
+        {
+            $oldLangPack = $this->resouceManagerInstance->getLanguagePack();
+
+            $newLangPackKeys = array_keys($oldLangPack);
+            foreach ($newLangPackKeys as $key)
+            {
+                if(!isset($oldLangPack[$key]))
+                    $oldLangPack[$key] = $this->getDefaultLanguagePack()[$key];
+            }
+
+            $oldLangPack['version'] = $this->resouceManagerInstance->getPluginVersion();
+
+            $this->resouceManagerInstance->saveLanguagePack($oldLangPack);
+        }
     }
 }
