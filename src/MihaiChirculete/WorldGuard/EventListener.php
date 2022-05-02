9685e6e71217cd70dea1492102f7ee60d825cc38
suerion <55131959+suerion@users.noreply.github.com>
2022-05-02 21:48:29 +0200

    added blocklegacyid class

--- src/MihaiChirculete/WorldGuard/EventListener.php
+++ src/MihaiChirculete/WorldGuard/EventListener.php
@@ -22,7 +22,7 @@
 
 namespace MihaiChirculete\WorldGuard;
 
-use pocketmine\block\Block;
+use pocketmine\block\{BlockLegacyIds, Block};
 use pocketmine\event\block\{BlockPlaceEvent, BlockBreakEvent, LeavesDecayEvent, BlockGrowEvent, BlockUpdateEvent, BlockSpreadEvent, BlockBurnEvent};
 use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent, EntityExplodeEvent, ProjectileLaunchEvent};
 use pocketmine\event\Listener;
