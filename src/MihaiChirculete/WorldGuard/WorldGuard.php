4049d1ce44b3e32e2b97fd0ed3143cfeb1beb1f1
Covered123 <58715544+JavierLeon9966@users.noreply.github.com>
2022-05-16 01:19:06 -0300

    Fixed crash when fly mode isn’t vanilla

--- src/MihaiChirculete/WorldGuard/WorldGuard.php
+++ src/MihaiChirculete/WorldGuard/WorldGuard.php
@@ -346,7 +346,7 @@ class WorldGuard extends PluginBase
                 }
                 if (! $player->hasPermission("worldguard.bypass.fly." . $newregion)) {
                     if (($flight = $new->getFlight()) !== self::FLY_VANILLA) {
-                        if ($player->getGamemode() != 1) {
+                        if (!$player->getGamemode()->equals(GameMode::CREATIVE())) {
                             switch ($flight) {
                                 case self::FLY_ENABLE:
                                 case self::FLY_SUPERVISED:
