54742e8b1ad431f397fff2a5dcef6fb90c27670e
suerion <55131959+suerion@users.noreply.github.com>
2022-05-29 17:58:07 +0200

    OP should not change Gamemode

--- src/MihaiChirculete/WorldGuard/WorldGuard.php
+++ src/MihaiChirculete/WorldGuard/WorldGuard.php
@@ -318,18 +318,20 @@ class WorldGuard extends PluginBase
                     }
                 }
                 if (($gm = $new->getGamemode()) !== $player->getGamemode()) {
-                    if (! $player->hasPermission("worldguard.bypass.gamemode." . $newregion)) {
-                        if ($gm !== "false") {
-                            if ($gm == "creative") {
-                                $player->setGamemode(GameMode::CREATIVE());
-                            } else if ($gm == "survival") {
-                                $player->setGamemode(GameMode::SURVIVAL());
-                                Utils::disableFlight($player);
-                            } else if ($gm == "adventure") {
-                                $player->setGamemode(GameMode::ADVENTURE());
-                                Utils::disableFlight($player);
-                            } else if ($gm == "spectator") {
-                                $player->setGamemode(GameMode::SPECTATOR());
+                    if (!$player->hasPermission(DefaultPermissions::ROOT_OPERATOR) {
+                        if (! $player->hasPermission("worldguard.bypass.gamemode." . $newregion)) {
+                            if ($gm !== "false") {
+                                if ($gm == "creative") {
+                                    $player->setGamemode(GameMode::CREATIVE());
+                                } else if ($gm == "survival") {
+                                    $player->setGamemode(GameMode::SURVIVAL());
+                                    Utils::disableFlight($player);
+                                } else if ($gm == "adventure") {
+                                    $player->setGamemode(GameMode::ADVENTURE());
+                                    Utils::disableFlight($player);
+                                } else if ($gm == "spectator") {
+                                    $player->setGamemode(GameMode::SPECTATOR());
+                                }
                             }
                         }
                     }
